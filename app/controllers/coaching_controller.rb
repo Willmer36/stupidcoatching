468541887d6e14136d5a64e61a051f871ab75597
Guillaume Grignard <a1z2e3r4t5y67890@gmail.com>
2016-02-15 15:05:09 +0100

    relou

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,15 @@
+class CoachingController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    params[:question]
+    if params[:question] == "j'y vais"
+      @answer = 'BRAVO tu déchires'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
